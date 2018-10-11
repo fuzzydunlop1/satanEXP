758c3309db2770fee069e7cb8952fd6e7cb0ffe9
fuzzydunlop1 <34413736+fuzzydunlop1@users.noreply.github.com>
2018-10-11 17:01:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,32 +23,30 @@ const comments = client.CommentStream(streamOpts);
 let rawdata = fs.readFileSync('data.json')
 let data = JSON.parse(rawdata)
 
-let exp = data.exp;
-let level = data.level;
-let nextMult = data.nextMult;
-let nextExp = level * nextMult;
+let satanexp = data.satanexp;
+let godexp = data.godexp;
 
-const reply = (comment, count) => {
+const satanreply = (comment, count) => {
 
-    if (exp < nextExp) {
-        return `/u/${comment.author.name} uses "Mormon". It's super effective!\n\nSatan is victorious. LV${level} Satan gains ${count} exp.\n\nExp until next level: ${exp}/${nextExp}\n\n ^(This is a bot. [Click here to find out what this is about.](https://www.nytimes.com/aponline/2018/10/07/us/ap-us-rel-mormon-conference-church-name.html))`        
-    } else if (exp >= nextExp) {
-        level++;
-        nextExp = level * 100
-        return `/u/${comment.author.name} uses "Mormon". It's super effective!\n\nSatan is victorious. Satan gains ${count} exp.\n\nSatan levels up to LV${level}! Exp until next level: ${exp}/${nextExp}\n\n ^(This is a bot. [Click here to find out what is this about.](https://www.nytimes.com/aponline/2018/10/07/us/ap-us-rel-mormon-conference-church-name.html))`
-    }
+            return `/u/${comment.author.name} uses "Mormon". This is a victory for Satan. Satan is now beating Mormon God {satanexp} to {godexp}\n\n ^(I am a bot. [Click here to find out what this is about.](https://www.nytimes.com/aponline/2018/10/07/us/ap-us-rel-mormon-conference-church-name.html))`        
+   
 }
 
-const saveData = (exp, level) => {
+const godreply = (comment, count) => {
+
+            return `/u/${comment.author.name} uses "Latter-day Saint". This is a victory for God. However, Satan is still beating Mormon God {satanexp} to {godexp}\n\n ^(I am a bot. [Click here to find out what this is about.](https://www.nytimes.com/aponline/2018/10/07/us/ap-us-rel-mormon-conference-church-name.html))`        
+   
+}
+
+const saveData = (satanexp, godexp) => {
     let data = JSON.stringify({
-        exp,
-        level,
-        nextMult 
+        satanexp,
+        godexp
     })
     fs.writeFileSync('data.json', data)
 }
 
-const checkFalseFlags = (comment) => {
+const checkGodFalseFlags = (comment) => {
     if (comment.body.toLowerCase().includes('mormont')){
         return false
     } else if (comment.author.name.toLowerCase() == 'satanexp' ) {
@@ -57,19 +55,48 @@ const checkFalseFlags = (comment) => {
         return false
     } else if (comment.body.toLowerCase().includes('mormon')) {
         return true
+    } else if (comment.body.toLowerCase().includes('latter-day saint')) {
+        return true
+    } else if (comment.body.toLowerCase().includes('latter day saint')) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const checkSatanFalseFlags = (comment) => {
+    if (comment.body.toLowerCase().includes('mormont')){
+        return false
+    } else if (comment.author.name.toLowerCase() == 'satanexp' ) {
+        return false
+    } else if (comment.body.toLowerCase().includes('exmormon')) {
+        return false
+    } else if (comment.body.toLowerCase().includes('mormon')) {
+        return true
+    } else if (comment.body.toLowerCase().includes('latter-day saint')) {
+        return true
+    } else if (comment.body.toLowerCase().includes('latter day saint')) {
+        return true
     } else {
         return false
     }
 }
 
 comments.on('comment', (comment) => {
-    if (checkFalseFlags(comment)) {
+    if (checkGodFalseFlags(comment)) {
         console.log(`****${comment.subreddit_name_prefixed} - ${comment.link_title}****`);
         console.log(`****${comment.body}**** \n`);
-        const count = (comment.body.toLowerCase().match(/mormon/g) || []).length
-        exp+=count;
-        saveData(exp, level);
+        godexp++
+        saveData(satanexp, godexp);
         const replyMessage = reply(comment, count)
         comment.reply(replyMessage)
     }   
+    if (checkSatanFalseFlags(comment)) {
+        console.log(`****${comment.subreddit_name_prefixed} - ${comment.link_title}****`);
+        console.log(`****${comment.body}**** \n`);
+        satanexp++
+        saveData(satanexp, godexp);
+        const replyMessage = reply(comment, count)
+        comment.reply(replyMessage)
+    } 
 })
